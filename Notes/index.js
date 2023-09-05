56cb9ca3dcc84edf79a8296029c8236fd99837c8
IvaH1992 <119977613+GrantReese@users.noreply.github.com>
2023-09-05 18:54:45 -0500

    Grasped Function Hoisting in JavaScript

--- Notes/index.js
+++ Notes/index.js
@@ -15,30 +15,38 @@
 //     console.log(a+c);
 // }
 // test3();
-var a = 1
-const b = 2
-hello1()
+// var a = 1
+// const b = 2
+// hello1()
 
 
-function hello1(){
-    console.log("Hello ");
+// function hello1(){
+//     console.log("Hello ");
    
+// }
+
+// math1()
+
+// function math1(){
+//     console.log(a + b);
+// }
+
+// function math2(){
+//     console.log(c + d);
+// }
+// //math2() will not work here
+
+// var c = 3
+// let d = 4
+
+// math2()
+
+//dog("Ramona");
+
+function dog(name){
+    console.log(`${name} is a very good dog!`);
 }
 
-math1()
-
-function math1(){
-    console.log(a + b);
+let dog = function(name){
+    console.log(`${name} is a very good dog!`);
 }
-
-function math2(){
-    console.log(c + d);
-}
-//math2() will not work here
-
-var c = 3
-let d = 4
-
-math2()
-
-
